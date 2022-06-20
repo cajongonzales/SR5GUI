58a360334c1ee27d05bf704f9f953ca4223f8ab8
cajon gonzales <cajongonzales@gmail.com>
2022-06-20 18:32:56 -0400

    Add spells and combat tabs to main page

--- GUI.py
+++ GUI.py
@@ -1,20 +1,56 @@
+from PyQt6.QtWidgets import (
+    QApplication,
+    QCheckBox,
+    QLabel,
+    QMainWindow,
+    QStatusBar,
+    QToolBar,
+    QHBoxLayout,
+    QWidget,
+    QTabWidget,
+)
+
+ 
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("SR5 Rulebook GUI")
+        self.resize(600,600)
+        # Main tab bar
+        tabs = QTabWidget(self)
+        spells_page = Spells()
+        tabs.addTab(spells_page,"Spells")
+        #spells_menu.addAction(button_action)
+        combat_page = Combat()
+        tabs.addTab(combat_page,"Combat")
+        self.setCentralWidget(tabs)
+
+class Spells (QWidget):
+    def __init__(self) -> None:
+        super().__init__()
+        layout = QHBoxLayout()
+        label = QLabel("Spells go here")
+        layout.addWidget(label)
+        self.setLayout(layout)
+
+class Combat(QWidget):
+    def __init__(self) -> None:
+        super().__init__()
+        layout = QHBoxLayout()
+        label = QLabel("Combat POW")
+        layout.addWidget(label)
+        self.setLayout(layout)
 
 def main():
-    from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget
-    from PyQt6.QtGui import QPalette, QColor
 
     app = QApplication([])
 
-    # Create a Qt widget, which will be our window.
-    mw = QMainWindow()
+    # Create the main window
+    mw = MainWindow()
     mw.show()
-
+    
     # Start the event loop.
     app.exec()
-
-    # Your application won't reach here until you exit and the event
-    # loop has stopped.
-
 
 if __name__ == '__main__':
     main()
